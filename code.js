5420db628488f9583729a93bc7ac2c41638d7b94
Storm <yannic.freson1@gmail.com>
2020-09-30 17:25:13 +0200

    upd

--- code.js
+++ code.js
@@ -1,13 +1,31 @@
 let input = prompt('What do you wanna SpOnGeBoBiFy ?')
 let output = ''
 
-for (i = 0; i < input.length; i++) {
-    let upperCase = (Math.random() < 0.5)
-    if (upperCase) {
-        output += input.charAt(i).toUpperCase()
+function spongebobify(random) {
+    if (random) {
+        for (i = 0; i < input.length; i++) {
+            let upperCase = (Math.random() < 0.5)
+            if (upperCase) {
+                output += input.charAt(i).toUpperCase()
+            } else {
+                output += input.charAt(i).toLowerCase()
+            }
+        }
     } else {
-        output += input.charAt(i).toLowerCase()
+        for (i = 0; i < input.length; i++) {
+            if (i % 2 === 0) {
+                output += input.charAt(i).toUpperCase()
+            } else if (i % 2 !== 0) {
+                output += input.charAt(i).toLowerCase()
+            }
+        }
     }
 }
 
+spongebobify
+
 document.getElementById('Output').textContent = output
+
+document.addEventListener('click', function (click) {
+    if (click.target.classList.contains('Random'))
+})
