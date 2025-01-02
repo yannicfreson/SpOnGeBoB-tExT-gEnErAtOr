63727bb79732a34125b942629c77ae67ef0a205d
yannicfreson <61149527+yannicfreson@users.noreply.github.com>
2025-01-02 14:15:19 +0100

    Neumorphism theme practice

--- code.js
+++ code.js
@@ -1,34 +1,70 @@
-let input = document.getElementById('inputText').value
-let output = ''
+function spongebobify(random, input) {
+    if (input === undefined) {
+        input = document.getElementById('inputText').value
+    }
 
-function spongebobify(random) {
-    input = document.getElementById('inputText').value
-    output = ''
+    if (input === '') {
+        input = "Enter text"
+        document.getElementById('outputText').style.color = "#757575"
+    }
+
+    let output = ''
+
     if (random) {
         for (i = 0; i < input.length; i++) {
             let upperCase = (Math.random() < 0.5)
-            if (upperCase) {
-                output += input.charAt(i).toUpperCase()
+            if (input.charAt(i) === ' ') {
+                output += ' '
             } else {
-                output += input.charAt(i).toLowerCase()
+                if (input.charAt(i).toLowerCase() === 'l') {
+                    output += 'L'
+                } else if (input.charAt(i).toLowerCase() === 'i') {
+                    output += 'i'
+                } else {
+                    if (upperCase) {
+                        output += input.charAt(i).toUpperCase()
+                    } else {
+                        output += input.charAt(i).toLowerCase()
+                    }
+                }
             }
         }
     } else {
+        let upperCase = true;
         for (i = 0; i < input.length; i++) {
-            if (i % 2 === 0) {
-                output += input.charAt(i).toUpperCase()
-            } else if (i % 2 !== 0) {
-                output += input.charAt(i).toLowerCase()
+            if (input.charAt(i) === ' ') {
+                output += ' '
+            } else {
+                if (input.charAt(i).toLowerCase() === 'l') {
+                    output += 'L'
+                    upperCase = false
+                } else if (input.charAt(i).toLowerCase() === 'i') {
+                    output += 'i'
+                    upperCase = true
+                } else {
+                    if (upperCase) {
+                        output += input.charAt(i).toUpperCase()
+                        upperCase = false
+                    } else {
+                        output += input.charAt(i).toLowerCase()
+                        upperCase = true
+                    }
+                }
             }
         }
     }
-    document.getElementById('output').textContent = output
+    return output
 }
 
 document.addEventListener('click', click => {
     if (click.target == document.getElementById('random')) {
-        spongebobify(true)
+        document.getElementById('outputText').style.color = "#000"
+        document.getElementById('outputText').innerHTML = spongebobify(true)
     } else if (click.target == document.getElementById('alternate')) {
-        spongebobify(false)
+        document.getElementById('outputText').style.color = "#000"
+        document.getElementById('outputText').innerHTML = spongebobify(false)
     }
 })
+
+document.getElementById('title').innerHTML = spongebobify(false, 'Spongebob text generator')
+document.getElementById('outputText').style.color = "#757575"
